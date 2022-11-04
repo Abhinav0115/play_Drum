67e8d388123e59b1f5006b306b878e81c6aeddda
Abhinav Anand <abhinav011501@gmail.com>
2022-11-04 13:48:24 +0530

    Update drum_script.js

--- drum_script.js
+++ drum_script.js
@@ -20,37 +20,37 @@ function playDrum(key) {
     switch (key) {
         case "j":
         case "J":
-            var audio = new Audio("sound/crash.mp3");
+            var audio = new Audio("../sound/crash.mp3");
             audio.play();
             break;
         case "k":
         case "K":
-            var audio = new Audio("sound/kick-bass.mp3");
+            var audio = new Audio("../sound/kick-bass.mp3");
             audio.play();
             break;
         case "l":
         case "L":
-            var audio = new Audio("sound/snare.mp3");
+            var audio = new Audio("../sound/snare.mp3");
             audio.play();
             break;
         case "w":
         case "W":
-            var audio = new Audio("sound/tom-1.mp3");
+            var audio = new Audio("../sound/tom-1.mp3");
             audio.play();
             break;
         case "a":
         case "A":
-            var audio = new Audio("sound/tom-2.mp3");
+            var audio = new Audio("../sound/tom-2.mp3");
             audio.play();
             break;
         case "s":
         case "S":
-            var audio = new Audio("sound/tom-3.mp3");
+            var audio = new Audio("../sound/tom-3.mp3");
             audio.play();
             break;
         case "d":
         case "D":
-            var audio = new Audio("sound/tom-4.mp3");
+            var audio = new Audio("../sound/tom-4.mp3");
             audio.play();
             break;
 
